a30c5cf6f60d5b37c497a6ad3599fe1ae3784e02
AzizL <azizlokhandwala12@gmail.com>
2025-12-16 16:28:07 +0530

    Add player data subsystem

---
+++ Source/BBattle/Public/Subsystems/PlayerDataSubSystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "PlayerDataSubSystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BBATTLE_API UPlayerDataSubSystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
