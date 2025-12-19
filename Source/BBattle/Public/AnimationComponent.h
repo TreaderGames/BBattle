4bf99372c610c30ba00d4c4e062c8b98b1ea9b6f
AzizL <azizlokhandwala12@gmail.com>
2025-12-19 17:26:43 +0530

    Add Animation component

---
+++ Source/BBattle/Public/AnimationComponent.h
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "AnimationComponent.generated.h"
+
+
+UENUM(BlueprintType)
+enum class AnimationKey : uint8
+{
+	None,
+	MeleeAttack UMETA(DisplayName = "Melee Attack"),
+};
+
+UDELEGATE(BlueprintCallable)
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnAnimationPlayed);
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BBATTLE_API UAnimationComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UAnimationComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(EditAnywhere, BlueprintAssignable, Category = "Ability");
+	FOnAnimationPlayed OnAnimationPlayed;
+
+	UFUNCTION(BlueprintCallable, meta = (CompactNodeTitle = "PlayAnimation"))
+	void PlayAnimation(AnimationKey key);
+
+	UFUNCTION(BlueprintCallable, meta = (CompactNodeTitle = "GetAnimationKey"))
+	AnimationKey GetAnimKey();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ability");
+	AnimationKey animKey;
+		
+};
