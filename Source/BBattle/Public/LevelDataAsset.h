30280d7717cc49733c94a2de310297cf0e72bc4a
AzizL <azizlokhandwala12@gmail.com>
2026-01-12 14:21:39 +0530

    Add leveldata config

---
+++ Source/BBattle/Public/LevelDataAsset.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "Abilities/AbilityData.h"
+#include "LevelDataAsset.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FEnemyData
+{
+	GENERATED_BODY();
+
+	UPROPERTY(EditAnywhere, Category = "Ability")
+	TArray<FAbilityData> abilityArr;
+};
+/**
+ * 
+ */
+UCLASS(BlueprintType)
+class BBATTLE_API ULevelDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+
+	UPROPERTY(EditAnywhere, Category = "Ability")
+	TArray <FEnemyData> enemyData;
+
+};
