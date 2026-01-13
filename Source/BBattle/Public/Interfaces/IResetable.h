bf791067cb0ca96f83defbe5864905955b293ac3
AzizL <azizlokhandwala12@gmail.com>
2026-01-13 15:13:17 +0530

    Add resetable interface

---
+++ Source/BBattle/Public/Interfaces/IResetable.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+//#include "Interfaces/IResetable.generated.h"
+#include "IResetable.generated.h"
+/**
+ * 
+ */
+UINTERFACE(MinimalAPI, Blueprintable)
+class UResetable : public UInterface
+{
+	GENERATED_BODY()
+
+};
+
+class IResetable
+{
+	GENERATED_BODY()
+
+public:
+
+	void Reset();
+
+};
