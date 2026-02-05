a6f97fda71622dfdc8f5f007c26ad1116960230a
AzizL <azizlokhandwala12@gmail.com>
2026-02-05 14:49:31 +0530

    Add spin attack to data

---
+++ Source/BBattle/Public/SlashAbility.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilityBase.h"
+#include "SlashAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BBATTLE_API USlashAbility : public UAbilityBase
+{
+	GENERATED_BODY()
+	
+};
